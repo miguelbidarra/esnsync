29a5845be0d51b00b16beefccd578050b7f6de9a
miguelbidarra <miguelbidarrab@gmail.com>
2024-05-05 00:40:36 +0100

    simple fix

--- esnsync-demo/src/auth/ProtectedRoute.jsx
+++ esnsync-demo/src/auth/ProtectedRoute.jsx
@@ -1,42 +1,20 @@
-import React from 'react';
-import { Outlet, Navigate } from 'react-router-dom';
-
-import UserContext from './UserContext';
-import { getLoggedInUser } from './auth';
+import React, { useContext } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import UserContext from "./UserContext";
+import { getLoggedInUser } from "./auth";
 
 export default function ProtectedRoute() {
-  const { user, setUser } = React.useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-  /*
-    if user naviagates to dashboard
-    check if the user is logged in.
-  */
-  if (user === null) {
-    /*
-      incase user closed the window
-      without logging out
-      try to retrieve the user
-    */
+  if (!user) {
     setUser(getLoggedInUser());
-
-    /*
-      if user did not previously login
-      before closing tab or window
-      redirect user to login page
-    */
-    if (user === null) {
-      return (
-        <Navigate to="/" replace />
-      );
-    }
   }
 
-  /*
-    if the user is logged in
-    redirect to dashbaord
-    which is currently the child of
-    PrivateRoute
-  */
+  if (!user) {
+    navigate("/", { replace: true });
+    return null;
+  }
 
-  return (<Outlet />);
+  return <Outlet />;
 }
